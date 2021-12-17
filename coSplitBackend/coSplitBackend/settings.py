dddf4a1676100610ebbea99950c59e1f515a7b0b
Yura <y0507yavor@gmail.com>
2021-12-17 14:49:54 +0200

    Created User migrations.

--- coSplitBackend/coSplitBackend/settings.py
+++ coSplitBackend/coSplitBackend/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,4 +130,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'apps.use.User'
+AUTH_USER_MODEL = 'user.User'
